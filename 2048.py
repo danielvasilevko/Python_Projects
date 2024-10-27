fbb07e9b9c57656d89373d2a248d8493f238b350
Daniel Vasilevko <danielvasilevko@email.com>
2024-10-27 12:23:22 -0700

    Will begin work on a function that randomly inserts a 2 or 4 in the 4x4 array.

--- 2048.py
+++ 2048.py
@@ -2,12 +2,20 @@ import random
 
 import numpy as np 
 
+def insert_num(array): #when given an array this function will insert a 2 or 4 randomly at any 0 location
+    return
+
+
 def game_2048():
     print("W to move up")
     print("S to move down")
     print("A to move left")
     print("D to move right")
 
+    
+board = np.zeros((4, 4))
+print(board)
 
-game_2048()
+
+
     

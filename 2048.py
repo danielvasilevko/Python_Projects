0c25e8c0b241862a88d44b12a8c0f23f2893b615
Daniel Vasilevko <danielvasilevko@email.com>
2024-10-26 19:25:28 -0700

    Set up the instructions and numpy. Planning to print and work with a 4x4 np.array as the game board.

---
+++ 2048.py
@@ -0,0 +1,13 @@
+import random
+
+import numpy as np 
+
+def game_2048():
+    print("W to move up")
+    print("S to move down")
+    print("A to move left")
+    print("D to move right")
+
+
+game_2048()
+    

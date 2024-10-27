067b2c7c0ac30fa04c1a3275e561d8c787b88bcb
Daniel Vasilevko <danielvasilevko@email.com>
2024-10-27 12:53:47 -0700

    Worked on insert num. Can now randomly pick location in array that has 0.

--- 2048.py
+++ 2048.py
@@ -3,7 +3,15 @@ import random
 import numpy as np 
 
 def insert_num(array): #when given an array this function will insert a 2 or 4 randomly at any 0 location
-    return
+    iterate = range(1,4)
+    locations = []
+    for i in iterate:
+        for j in iterate:
+            if array[i,j] == 0:
+                locations.append([i,j])
+    
+    insert_loc = random.choice(locations)
+    print(insert_loc)
 
 
 def game_2048():
@@ -12,10 +20,7 @@ def game_2048():
     print("A to move left")
     print("D to move right")
 
-    
-board = np.zeros((4, 4))
-print(board)
+board = np.zeros((4, 4)) 
 
+insert_num(board)
 
-
-    
